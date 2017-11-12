e71659b71a3d6025839408adf278302a15cb7828
David Swersky <david.swersky@rma.usda.gov>
2017-11-12 14:54:48 -0600

    Set comparison between Master and Next lists

--- listvalidator.py
+++ listvalidator.py
@@ -1,20 +1,30 @@
 from bs4 import BeautifulSoup
 import os
 
+testList = ["Foo","Bar","Bump","Bish"]
+masterList = [] 
+nextList = []
 with open(os.getcwd() + "\web-storelist-k7\storelist.htm") as fp:
     soup = BeautifulSoup(fp, "html.parser")
     
-    print("SECTIONS:")
-    for content in soup.find_all("ul", {"id":"grocerySections"}):
-        if content.text:
-            print (content.text)
     
-    print("MASTERLIST:")
+    print("SECTIONS:")
+    # for content in soup.find_all("ul", {"id":"grocerySections"}):
+    #     if content.text:
+    #         print (content.text)
+    
+    #print("MASTERLIST:")
     for content in soup.find_all("ol", {"id":"masterList"}):
         if content.text:
-            print(content.text)
+            masterList = content.text.splitlines()
     
-    print("NEXTVISIT:")
+    #print("NEXTVISIT:")
     for content in soup.find_all("ol",{"id":"nextList"}):
         if content.text:
-            print(content.text)
+            nextList = content.text.splitlines()
+
+    #print("MISMATCH:")
+print(testList)
+print(len(masterList))
+print(len(nextList))
+print(list(set(nextList) - set(masterList)))
